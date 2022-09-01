6ed92ade1837c74b91ebc1b50ad43ef1bb3eec93
brianP1201 <brianpauldc@gmail.com>
2022-08-31 23:45:53 -0400

    Initial Commit Assignment #1

---
+++ large-scale-programming-assignments/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * @author Brian Paul
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Large Scale Programming!!");
+	}
+
+}
